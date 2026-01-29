3a281f40bd25f1f02c026520964bc1817cc01b06
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2026-01-29 14:40:04 +0200

    apply lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,22 @@
 import { Routes } from '@angular/router';
 import { UserlayoutComponent } from './core/layouts/components/userlayout/userlayout.component';
 import { GuestlayoutComponent } from './core/layouts/components/guestlayout/guestlayout.component';
-import { NotfoundComponent } from './shared/components/notfound/notfound.component';
 import { LoginComponent } from './core/auth/login/login.component';
-import { SignupComponent } from './core/auth/signup/signup.component';
 import { HomeComponent } from './features/pages/home/home.component';
-import { CategoriesComponent } from './features/pages/categories/categories.component';
-import { BrandsComponent } from './features/pages/brands/brands.component';
-import { CartComponent } from './features/pages/cart/cart.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "", component: GuestlayoutComponent,children: [
         {path: "login", component: LoginComponent},
-        {path: "signup", component: SignupComponent},
+        {path: "signup", loadComponent:()=> import('./core/auth/signup/signup.component').then(c=>c.SignupComponent)},
     ]},
     {path: "", component: UserlayoutComponent,children: [
         {path: "home", component: HomeComponent},
-        {path: "categories", component: CategoriesComponent},
-        {path: "brands", component: BrandsComponent},
-        {path: "shopping-cart", component: CartComponent},
+        {path: "categories", loadComponent:()=> import('./features/pages/categories/categories.component').then(c=>c.CategoriesComponent)},
+        {path: "brands", loadComponent:()=> import('./features/pages/brands/brands.component').then(c=>c.BrandsComponent)},
+        {path: "cart", loadComponent:()=> import('./features/pages/cart/cart.component').then(c=>c.CartComponent)},
+        {path: "product-details", loadComponent:()=> import('./shared/components/productdetails/productdetails.component').then(c=>c.ProductdetailsComponent)},
     ]},
-    {path: "**", component: NotfoundComponent},
+    {path: "**", loadComponent:()=> import('./shared/components/notfound/notfound.component').then(c=>c.NotfoundComponent)},
 ];
  
