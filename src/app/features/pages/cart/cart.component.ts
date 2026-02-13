5771305ca736d2f02a5173436dc1c37a98d1636e
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2026-02-13 03:41:34 +0200

    shopping cart design is done

--- src/app/features/pages/cart/cart.component.ts
+++ src/app/features/pages/cart/cart.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [ButtonModule, RatingModule, TableModule, TagModule, FormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
-
+  
 }
